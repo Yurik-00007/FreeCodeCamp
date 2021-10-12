ed5621d42a2fd9e346d69cf5a2ad200a7b4a071c
Yurik <yurik-007@mail.ru>
2021-10-12 16:46:52 +0300

    less 6

---
+++ 6.js
@@ -0,0 +1,15 @@
+// Only change code below this line
+var a;
+var b;
+var c;
+// Only change code above this line
+a=5;
+b=10;
+c="I am a";
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+console.log(a);
+console.log(b);
+console.log(c);
