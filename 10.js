b7e0434ad35a2a241899bc63bbd7b202fc1d09e6
Yurik <yurik-007@mail.ru>
2021-10-12 18:10:04 +0300

    less 10-20

---
+++ 10.js
@@ -0,0 +1,75 @@
+var product = 8 * 10;
+
+console.log(product);
+//11
+var quotient = 66 / 33;
+
+console.log(quotient);
+//12
+var myVar = 87;
+
+// Only change code below this line
+myVar++;
+console.log(myVar);
+//13
+var myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+console.log(myVar);
+//14
+var myDecimal = 5.7;
+console.log(myDecimal);
+// Only change code below this line
+//15
+var product = 2.0 * 2.5;
+console.log(product);
+//16
+var quotient = 4.4 / 2.0; // Change this line
+console.log(quotient);
+//17
+// Only change code below this line
+
+var remainder=11%3;
+console.log(remainder);
+//18
+var a = 3;
+var b = 17;
+var c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+console.log(a);
+console.log(b);
+console.log(c);
+
+//19
+var a = 5;
+var b = 12;
+var c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+//20
+var a = 48;
+var b = 108;
+var c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+console.log(a);
+console.log(b);
+console.log(c);
