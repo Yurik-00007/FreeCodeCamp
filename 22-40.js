fdb865a160158d07d50de225810b2b2ff0f6938f
Yurik <yurik-007@mail.ru>
2021-10-16 19:12:54 +0300

    22-40

---
+++ 22-40.js
@@ -0,0 +1,16 @@
+//22
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+// Change this line
+console.log(myStr);
+
+//23
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+console.log(myStr);
+
+//24
+var myStr='FirstLine\n\t\\\SecondLine\nThirdLine'; 
+console.log(myStr);
+
+//25
+var myStr = 'This is the start. '+'This is the end.'; // Change this line
+console.log(myStr);
