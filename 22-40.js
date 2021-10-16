8594db505e0821046a30b18595d766e00848d0ed
Yurik <yurik-007@mail.ru>
2021-10-16 20:00:49 +0300

    22-36

--- 22-40.js
+++ 22-40.js
@@ -14,3 +14,95 @@ console.log(myStr);
 //25
 var myStr = 'This is the start. '+'This is the end.'; // Change this line
 console.log(myStr);
+
+//26
+// Only change code below this line
+
+var myStr = 'this is the first sentence. ';
+myStr +="This is the second sentence.";
+console.log(myStr);
+
+//27
+// Only change code below this line
+var myName = 'Yurik';
+var myStr = 'My name is '+myName+' and I\'m well!';
+console.log(myStr);
+//28
+// Change code below this line
+var namber=28;
+var someAdjective = 'awesome';
+var myStr = "Learning to code is ";
+myStr += someAdjective;
+console.log(namber+'\n'+myStr);
+//29
+// Setup
+var namber=29;
+var lastNameLength = 0;
+var lastName = "Lovelace";
+
+// Only change code below this line
+
+lastNameLength = lastName.length;
+console.log(namber+'\n'+lastNameLength);
+//30
+// Setup
+var namber=30;
+var firstLetterOfLastName = "";
+var lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+console.log(namber+'\n'+firstLetterOfLastName);
+
+//31
+// Setup
+var namber=31;
+var myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+console.log(namber+'\n'+myStr);
+
+//32
+// Setup
+var namber=32;
+var lastName = "Lovelace";
+
+// Only change code below this line
+var thirdLetterOfLastName = lastName[2]; // Change this line
+console.log(namber+'\n'+thirdLetterOfLastName);
+//33
+var namber=33;
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var lastLetterOfLastName = lastName[lastName.length-1]; // Change this line
+console.log(namber+'\n'+lastLetterOfLastName);
+
+//34
+var namber=34;
+// Setup
+var lastName = "Lovelace";
+
+// Only change code below this line
+var secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+console.log(namber+'\n'+secondToLastLetterOfLastName);
+//35
+var namber=35;
+var myNoun = "dog";
+var myAdjective = "big";
+var myVerb = "ran";
+var myAdverb = "quickly";
+
+// Only change code below this line
+var wordBlanks = "I saw a "+myNoun+". It was very "+myAdjective+". I was afrade of it and I "+myVerb+" "+myAdverb+"."; // Change this line
+// Only change code above this line
+console.log(namber+'\n'+wordBlanks);
+
+//36
+// Only change code below this line
+var namber=36;
+var myArray = ['sanny',5,'nice'];
+console.log(namber+'\n'+myArray);
