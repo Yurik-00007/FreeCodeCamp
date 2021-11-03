65b638022ae43a0a15c96c71a7b432fa9357bbcc
Yurik <yurik-007@mail.ru>
2021-11-03 12:25:08 +0300

    59

--- 50-70.js
+++ 50-70.js
@@ -3,7 +3,7 @@ var outerWear = "T-Shirt";
 
 function myOutfit() {
   // Only change code below this line
-let outerWear='sweater'
+let outerWear='sweater';
 
 
   // Only change code above this line
@@ -57,8 +57,90 @@ processed=processArg(7);
 // Only change code below this line
 console.log(processed);
 
+
 //54
 
+//54 Изучите различия между ключевыми словами var и let
+let catName = "Oliver";
+catName = "Swen";
+let catSound = "Meow!";
+console.log(catName);
+console.log(catSound);
 
-//55
+//55 Объявите переменную только для чтения с помощью ключевого слова const
 
+// Only change code below this line
+const FCC = "freeCodeCamp";
+let fact = "is cool!";
+// Only change code above this line
+
+fact = "is awesome!";
+console.log(FCC, " ", fact);
+
+//56 Очередь
+function nextInLine(arr, item) {
+  // Only change code below this line
+  arr.push(item);
+  var temp = arr.shift();
+  return temp;
+  // Only change code above this line
+}
+
+// Setup
+const testArr = [1, 2, 3, 4, 5];
+// Display code
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
+
+//57 Понимание логических значений
+
+function welcomeToBooleans() {
+  // Only change code below this line
+
+  return true; // Change this line
+
+  // Only change code above this line
+}
+
+console.log(welcomeToBooleans());
+
+//58 Использование условной логики с операторами If
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue){
+  return "Yes, that was true";
+}
+return "No, that was false"
+
+
+  // Only change code above this line
+
+}
+console.log(trueOrFalse(true));
+console.log(trueOrFalse(false));
+
+//59
+
+//60
+
+//61
+
+//62
+
+//63
+
+//64
+
+//65
+
+//66
+
+//67
+
+//68
+
+//69
+
+//70
