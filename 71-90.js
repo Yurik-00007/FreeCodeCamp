1475fd8553d3bd5f42159666908fad5d25077927
Yurik <yurik-007@mail.ru>
2021-11-03 19:57:02 +0300

    71

---
+++ 71-90.js
@@ -0,0 +1,49 @@
+//71
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }else if (val < 5) {
+      return "Smaller than 5";
+    }else{
+      return "Between 5 and 10";
+    }
+  }
+  
+  console.log(testElseIf(7));
+//72
+
+//73
+
+//74
+
+//75
+
+//76
+
+//77
+
+//78
+
+//79
+
+//80
+
+//81
+
+//82
+
+//83
+
+//84
+
+//85
+
+//86
+
+//87
+
+//88
+
+//89
+
+//90
