b80961b7b8938ad0bffc8f31914370edfe0f3610
Yurik <yurik-007@mail.ru>
2021-11-21 20:25:32 +0300

    93

---
+++ 90-100.js
@@ -0,0 +1,56 @@
+//91
+const myMusic = [
+    {
+      "artist": "Billy Joel",
+      "title": "Piano Man",
+      "release_year": 1973,
+      "formats": [
+        "CD",
+        "8T",
+        "LP"
+      ],
+      "gold": true
+    },
+     {
+      "artist": "Daft Punk",
+      "title": "Homework",
+      "release_year": 1997,
+      "formats": [
+        "CD",
+        "8T",
+        "LP",
+        "Cassette"
+      ],
+      "gold": true
+    },
+  ];
+//92
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
+    }
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
+  console.log(gloveBoxContents);
+//93
+
+//94
+
+//95
+
+//96
+
+//97
+
+//98
+
+//99
+
+//100
